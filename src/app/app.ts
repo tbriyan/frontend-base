85ddcc665d52db59c4e43f47b31d97a21b6a2ade
Briyan Julio Torrez <btorrez@aduana.gob.bo>
2025-08-20 09:21:45 -0400

    feat: agregar primeng

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { ProductService } from './app.service';
 
+export interface Product {
+  id?: string;
+  code?: string;
+  name?: string;
+  description?: string;
+  price?: number;
+  quantity?: number;
+  inventoryStatus?: string;
+  category?: string;
+  image?: string;
+  rating?: number;
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, TableModule],
+  providers: [ProductService],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'mi-app';
+
+  products!: any[];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProductsMini().then((data) => {
+      this.products = data;
+    });
+  }
+
+  obtenerProductos(){
+    this.productService.obtenerProductos().subscribe((data) => {
+      console.log(data);
+    })
+  }
 }
